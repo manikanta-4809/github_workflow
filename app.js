33057ee356973956e3b99a1fe571362ddefea4c0
manikanta-4809 <venkatamanikanta.sai@minfytech.com>
2025-06-21 17:47:26 +0530

    Update app.js
    token key

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
 app.get('/', (req, res) => {
-  res.send('Hello from my FIXED CI/CD pipeline!');
-});
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  res.send('Hello from my FIXED CI/CD pipeline with GHCR_TOKEN!');
 });

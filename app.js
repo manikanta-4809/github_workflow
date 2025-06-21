1ae9a3f70d4969adb8e4092a497da51c117b2020
manikanta-4809 <venkatamanikanta.sai@minfytech.com>
2025-06-21 15:44:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
 app.get('/', (req, res) => {
-  res.send('Hello from our containerized Node.js App!');
-});
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  res.send('Hello from my FINAL CI/CD pipeline!');
 });
